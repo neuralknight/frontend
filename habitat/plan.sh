c80d2b8faebf3b7b0958bc87abfe87372bbabce1
Adam Grandquist <grandquista@gmail.com>
2018-08-13 08:29:13 -0700

    Fix warnings in shell script.

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -1,4 +1,5 @@
-pkg_name=neuralknight-frontend
-pkg_origin=grandquista
-pkg_version="0.1.0"
-pkg_scaffolding="core/scaffolding-node"
+#!/bin/bash
+export pkg_name=neuralknight-frontend
+export pkg_origin=grandquista
+export pkg_version="0.1.0"
+export pkg_scaffolding="core/scaffolding-node"

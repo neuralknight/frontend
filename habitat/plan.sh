49cb7f38b37a274cdd66ac0392956b0ce6657912
Adam Grandquist <grandquista@gmail.com>
2018-08-21 14:38:35 -0700

    Fix build for using yarn.

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 export pkg_name=neuralknight-frontend
 export pkg_origin=grandquista
+export pkg_maintainer="Adam Grandquist <grandquista@gmail.com>"
 export pkg_version="0.1.0"
+export pkg_license=('MIT License')
 export pkg_scaffolding="core/scaffolding-node"
+
+do_unpack() {
+  return 0
+}
+
+do_build() {
+  yarn
+}
+
+do_install() {
+  return 0
+}
+
+do_check() {
+  yarn test
+}
